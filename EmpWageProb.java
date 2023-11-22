e750fbb095d92c91bfc06ce5e0b4d1fc280b7561
yoginpahuja <yoginpahuja69@gmail.com>
2023-11-23 01:35:54 +0530

    Added: Daily Wage Computation

--- EmpWageProb.java
+++ EmpWageProb.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class EmpWageProb {
-    public static final int isFullTime=1;
+    public static final int isFullTime=1,dailyHrs=8,dailyWage=20;
+
     public static double empCheck()
     {
         return Math.floor(Math.random()*10)%2;
@@ -8,10 +9,16 @@ public class EmpWageProb {
     public static void main(String[] args)
     {
         System.out.println("Welcome to Employee Wage Computation");
-
+        int empHrs=0;
+        int totalDailyWage=0;
         if(empCheck()==isFullTime)
-            System.out.println("Employee is Present");
+            {
+                System.out.println("Employee is Present");
+                empHrs+=dailyHrs;
+            }
         else
             System.out.println("Employee is Absent");
+        totalDailyWage=dailyWage*empHrs;
+        System.out.println("Daily wage computed is " + totalDailyWage);
     }
 }
