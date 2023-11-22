4c7b38ec58f4e5b4752d524d4ac33f3af549e2af
yoginpahuja <yoginpahuja69@gmail.com>
2023-11-23 01:11:32 +0530

    Added: Employee Present or Absent Functionality

--- EmpWageProb.java
+++ EmpWageProb.java
@@ -1,7 +1,17 @@
 import java.util.*;
 public class EmpWageProb {
+    public static final int isFullTime=1;
+    public static double empCheck()
+    {
+        return Math.floor(Math.random()*10)%2;
+    }
     public static void main(String[] args)
     {
         System.out.println("Welcome to Employee Wage Computation");
+
+        if(empCheck()==isFullTime)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
