c59a411c3e811516742b428c0202a45893d428ef
yoginpahuja <yoginpahuja69@gmail.com>
2023-11-23 02:48:14 +0530

    Refactor: Used Switch Case statement instead of if else

--- EmpWageProb.java
+++ EmpWageProb.java
@@ -2,27 +2,43 @@ import java.util.*;
 public class EmpWageProb {
     public static final int isFullTime=1,isPartTime=2,dailyHrs=8,dailyWage=20,partTimeHrs=4;
 
-    public static double empCheck()
+    public static int empCheck()
     {
-        return Math.floor(Math.random()*10)%3;
+        return (int)Math.floor(Math.random()*10)%3;
     }
     public static void main(String[] args)
     {
         System.out.println("Welcome to Employee Wage Computation");
         int empHrs=0;
         int totalDailyWage=0;
-        if(empCheck()==isFullTime)
-            {
-                System.out.println("Employee is Present");
+        // if(empCheck()==isFullTime)
+        //     {
+        //         System.out.println("Employee is Present");
+        //         empHrs+=dailyHrs;
+        //     }
+        // else if(empCheck()==isPartTime)
+        //     {
+        //            System.out.println("Employee is Present");
+        //            empHrs+=partTimeHrs;
+        //     }
+        // else
+        //     System.out.println("Employee is Absent");
+       
+
+        switch (empCheck()) {
+            case isFullTime:
+                System.out.println("Employee is Present (Full time)");
                 empHrs+=dailyHrs;
-            }
-        else if(empCheck()==isPartTime)
-            {
-                   System.out.println("Employee is Present");
-                   empHrs+=partTimeHrs;
-            }
-        else
-            System.out.println("Employee is Absent");
+                break;
+            case isPartTime:
+                System.out.println("Employee is Present (Part time)");
+                empHrs+=partTimeHrs;
+                break;
+            default:
+                System.out.println("Employee is Absent");
+                break;
+        }
+
         totalDailyWage=dailyWage*empHrs;
         System.out.println("Daily wage computed is " + totalDailyWage);
     }
