d041d72febc37de188843ae69385ae9dd9c5d504
yoginpahuja <yoginpahuja69@gmail.com>
2023-11-23 01:45:06 +0530

    Refactor: Part time wage Computation included

--- EmpWageProb.java
+++ EmpWageProb.java
@@ -1,10 +1,10 @@
 import java.util.*;
 public class EmpWageProb {
-    public static final int isFullTime=1,dailyHrs=8,dailyWage=20;
+    public static final int isFullTime=1,isPartTime=2,dailyHrs=8,dailyWage=20,partTimeHrs=4;
 
     public static double empCheck()
     {
-        return Math.floor(Math.random()*10)%2;
+        return Math.floor(Math.random()*10)%3;
     }
     public static void main(String[] args)
     {
@@ -15,6 +15,11 @@ public class EmpWageProb {
             {
                 System.out.println("Employee is Present");
                 empHrs+=dailyHrs;
+            }
+        else if(empCheck()==isPartTime)
+            {
+                   System.out.println("Employee is Present");
+                   empHrs+=partTimeHrs;
             }
         else
             System.out.println("Employee is Absent");
